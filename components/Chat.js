c290e3e7750cc45e41e349c6930f2af15b1bf0aa
Jessica Slusark <slusark.jessica@gmail.com>
2023-07-03 16:52:04 +0200

    Added Gifted Chat library and related functions

--- components/Chat.js
+++ components/Chat.js
@@ -1,13 +1,64 @@
 // chat page
-import { useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { useState, useEffect, useCallback } from "react";
+import {
+	StyleSheet,
+	View,
+	Text,
+	Platform,
+	KeyboardAvoidingView,
+} from "react-native";
+import { Bubble, GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
 	const { name, selectedColor } = route.params;
+	const [messages, setMessages] = useState([]);
 
 	useEffect(() => {
 		navigation.setOptions({ title: name });
+
+		setMessages([
+			{
+				_id: 1,
+				text: `Hello ${name}! 👋`,
+				createdAt: new Date(),
+				user: {
+					_id: 2,
+					name: "React Native",
+					avatar: "https://placeimg.com/140/140/any",
+				},
+			},
+			{
+				_id: 2,
+				text: "This is a system message",
+				createdAt: new Date(),
+				system: true,
+			},
+		]);
 	}, []);
+
+	// Function that adds a new message
+	const onSend = useCallback((messages = []) => {
+		setMessages((previousMessages) =>
+			GiftedChat.append(previousMessages, messages)
+		);
+	}, []);
+
+	// Function to customize the appearance of chat bubbles
+	const renderBubble = (props) => {
+		return (
+			<Bubble
+				{...props}
+				wrapperStyle={{
+					right: {
+						backgroundColor: `rgba(0, 0, 0, 0.2)`,
+					},
+					left: {
+						backgroundColor: "#ffffff",
+					},
+				}}
+			/>
+		);
+	};
 
 	return (
 		<View
@@ -16,15 +67,26 @@ const Chat = ({ route, navigation }) => {
 				{ backgroundColor: selectedColor.backgroundColor },
 			]}
 		>
-			<Text>Welcome {name}! 👋</Text>
+			<GiftedChat
+				messages={messages}
+				renderBubble={renderBubble}
+				onSend={(messages) => onSend(messages)}
+				user={{
+					_id: 1,
+				}}
+			/>
+			{Platform.OS === "android" ? (
+				<KeyboardAvoidingView behavior="height" />
+			) : null}
+			{Platform.OS === "ios" ? (
+				<KeyboardAvoidingView behavior="padding" />
+			) : null}
 		</View>
 	);
 };
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
 	},
 });
 
