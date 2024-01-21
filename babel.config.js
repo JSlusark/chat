9e93967a23b29695b025ec9d64fbc1c4c016ff7a
Jessica Slusark <slusark.jessica@gmail.com>
2024-01-21 19:00:35 +0100

    updated dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = function (api) {
+	api.cache(true);
+	return {
+		presets: ["babel-preset-expo"],
+	};
 };
+
+// native web conf https://teovillanueva.github.io/react-native-web-maps/installation/expo-web
